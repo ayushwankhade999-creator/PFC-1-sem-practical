c1832d611a54d4b1fca1f453616a4e6486fd300b
ayushwankhade999-creator <ayushwankhade999@gmail.com>
2025-12-25 15:34:32 +0530

    Add files via upload

---
+++ PRACTICAL 7/ABCD .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    //Ayush Wankhade
+    // 10359
+    int n = 4;
+    for(int i=1; i<=n; i++) {
+        for(char ch='A'; ch<'A'+i; ch++) {
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+// Output 
+// ; if ($?) { gcc abcd.c -o abcd } ; if ($?) { .\abcd }
+// A
+// AB
+// ABC
+// ABCD
